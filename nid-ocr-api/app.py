646dd8a9717e1106fa4b4738e780c2feb5580c5f
snanilim <snanilim@gmail.com>
2020-04-07 16:04:38 +0600

    image to ocr final push

--- nid-ocr-api/app.py
+++ nid-ocr-api/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, jsonify
 from datetime import datetime
-from process import imageToOcr, preImgToOcr
+from process import imageToOcr, processImgToOcr, base64_to_image
 from PIL import Image
 from io import BytesIO
 import re, time, base64
@@ -10,25 +10,49 @@ import cv2
 
 app = Flask(__name__, static_url_path='', static_folder='./',)
 
-
-# file formate check
-def file_formate_check(filename):
-    if filename.endswith("png") or filename.endswith("jpg") or filename.endswith("jpeg"):
-        return True
-    else:
-        return False
-
-
-def data_uri_to_cv2_img(uri):
-    encoded_data = uri.split(',')[1]
-    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    return img
-
-
 @app.route('/')
 def index():
     return jsonify({"message": "API Start"})
+
+
+
+@app.route('/img-to-ocr', methods=['GET', 'POST'])
+def imgToOcr():
+    if request.method == 'GET':
+        return jsonify({"result": "Its A post request Please use a 'POST' method"})
+
+    if request.method == 'POST':
+        try:
+            print('body', type(request.data))
+            # print('body', request.get_json()['nid_number'])
+            threshold = request.get_json()['threshold']
+            medianBlur = request.get_json()['medianBlur']
+            scale = request.get_json()['scale']
+            blurRatio = request.get_json()['blurRatio']
+            ocr_model = request.get_json()['ocr_model']
+            base64_image = request.get_json()['img_path']
+            img_path = "raw_image/raw_image.jpg"
+
+            base64_to_image(base64_image, img_path)
+
+
+            result = processImgToOcr(img_path, threshold, medianBlur, scale, blurRatio, ocr_model)
+            return jsonify({ "result": "Success", "data": result})
+        except Exception as error:
+            print('imgToOcr', error)
+            return jsonify({"is_success": False, "message": "Failed to convert image to ocr. please try again"})
+
+
+@app.route('/test-img-to-ocr', methods=['GET', 'POST'])
+def testImgToOcr():
+    if request.method == 'GET':
+        try:
+            img_path = "raw_image/test_raw_image.jpg"
+            result = processImgToOcr(img_path, 0, 1, 1, 1, ['ben', 'bengali'])
+            return jsonify({ "result": "Success", "data": result})
+        except Exception as error:
+            print('imgToOcr', error)
+            return jsonify({"is_success": False, "message": "Failed to convert image to ocr. Please try again"})
 
 
 @app.route('/upload-image', methods=['GET', 'POST'])
@@ -46,36 +70,13 @@ def uploadNid():
         base64_image = request.get_json()['img_path']
         img_path = "raw_image/raw_image.jpg"
 
-        image = data_uri_to_cv2_img(base64_image)
-        cv2.imwrite(img_path, image)
+        base64_to_image(base64_image, img_path)
 
 
         result = imageToOcr(img_path, threshold, medianBlur, blurRatio, ocr_model)
         return jsonify({ "result": "Success", "data": result})
         
 
-
-@app.route('/img-to-ocr', methods=['GET', 'POST'])
-def imgToOcr():
-    if request.method == 'GET':
-        return jsonify({"result": "Its A post request Please use a 'POST' method"})
-
-    if request.method == 'POST':
-        print('body', type(request.data))
-        # print('body', request.get_json()['nid_number'])
-        threshold = request.get_json()['threshold']
-        medianBlur = request.get_json()['medianBlur']
-        blurRatio = request.get_json()['blurRatio']
-        ocr_model = request.get_json()['ocr_model']
-        base64_image = request.get_json()['img_path']
-        img_path = "raw_image/raw_image.jpg"
-
-        image = data_uri_to_cv2_img(base64_image)
-        cv2.imwrite(img_path, image)
-
-
-        result = preImgToOcr(img_path, threshold, medianBlur, blurRatio, ocr_model)
-        return jsonify({ "result": "Success", "data": result})
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', port=3000)
--- nid-ocr-api/process.py
+++ nid-ocr-api/process.py
@@ -6,9 +6,108 @@ from pdf2image import convert_from_path
 import os
 import cv2
 from pathlib import Path
+import PIL
+from PIL import Image, ExifTags, ImageOps
+from io import BytesIO
+import re, time, base64
 
 # /usr/local/Cellar/tesseract/4.0.0_1/bin/tesseract [for mac]
 # /usr/share/tesseract-ocr/4.00/tessdata [for linux]
+
+def rotate_image(im):
+    try:
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation]=='Orientation':
+                break
+        exif=dict(im._getexif().items())
+        print('exif[orientation]', exif[orientation])
+        if exif[orientation] == 3:
+            im=im.rotate(180, expand=True)
+        elif exif[orientation] == 6:
+            im=im.rotate(270, expand=True)
+        elif exif[orientation] == 8:
+            im=im.rotate(90, expand=True)
+
+        return im
+    except Exception as error:
+        print('error_rotate_image', error)
+        return False
+
+def resize_image(image):
+    try:
+        basewidth = 650
+        if float(image.size[0]) > basewidth:
+            wpercent = (basewidth / float(image.size[0]))
+            hsize = int((float(image.size[1]) * float(wpercent)))
+            image = image.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+            return image
+        else:
+            return image
+    except Exception as error:
+        print('resize_image', error)
+        return False
+
+
+def base64_to_image(base64_image, img_path):
+    try:
+        # print('base64_image', base64_image)
+        encoded_data = base64_image.split(',')[1]
+        im = Image.open(BytesIO(base64.b64decode(encoded_data)))
+        image = rotate_image(im)
+        if image:
+            reImg = resize_image(image)
+        else:
+            reImg = resize_image(im)
+        reImg.convert('RGB').save(img_path)
+
+        return True
+    except Exception as error:
+        print('base64_to_image', error)
+        return error
+
+
+def processImgToOcr(filename, threshold, medianBlur, scale, blurRatio, ocr_model):
+    try:
+        image = cv2.imread(filename)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        if scale == 1:
+            print('scale')
+            gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+        if medianBlur == 1:
+            gray = cv2.medianBlur(gray, blurRatio)
+            gray = cv2.GaussianBlur(gray, (1, 1), 0)
+        if threshold == 1:
+            thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 139, 25.0)
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+            gray = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+        filename = 'process_image/image.jpg'
+        cv2.imwrite(filename, gray)
+
+        return_obj = {}
+        if "ben" in ocr_model:
+            print('ben----------')
+            ben = pytesseract.image_to_string(Image.open(filename), lang='ben')
+            ben_strings = ben.splitlines()
+            return_obj['ben'] = [x.strip() for x in ben_strings if x.strip()]
+
+        if "bengali" in ocr_model:
+            print('bengali----------')
+            bengali = pytesseract.image_to_string(Image.open(filename), lang='Bengali')
+            bengali_strings = bengali.splitlines()
+            return_obj['bengali'] = [x.strip() for x in bengali_strings if x.strip()]
+
+        return return_obj
+    except Exception as error:
+        print('processImgToOcr', error)
+        return error
+
+
+
+
+
+
 
 def imageToOcr(filename, threshold, medianBlur, blurRatio, ocr_model):
     try:
@@ -50,83 +149,37 @@ def imageToOcr(filename, threshold, medianBlur, blurRatio, ocr_model):
 
         return return_obj
     except Exception as error:
-        print('error', error)
+        print('error_imageToOcr', error)
+        return error
 
 
+# def onlyeng(filename, threshold, medianBlur, blurRatio, ocr_model):
+#     try:
+#         image = cv2.imread(filename)
 
-def preImgToOcr(filename, threshold, medianBlur, blurRatio, ocr_model):
-    try:
-        img = cv2.imread(filename)
-        img = cv2.bilateralFilter(img, 9, 31, 31)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#         gray = cv2.medianBlur(gray, blurRatio)
+#         gray = cv2.GaussianBlur(gray, (1, 1), 0)
+#         # gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+#         thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 139, 25.0)
+#         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+#         gray = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+#         # gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 139, 25.0)
         
-        gray = cv2.medianBlur(gray, 1)
-        gray = cv2.GaussianBlur(gray, (1, 1), 0)
-        thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 139, 25.0)
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
-        gray = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+#         filename = 'process_image/image.jpg'
+#         cv2.imwrite(filename, gray)
 
-        filename = 'process_image/image.jpg'
-        cv2.imwrite(filename, gray)
+#         return_obj = {}
+#         if "ben" in ocr_model:
+#             ben = pytesseract.image_to_string(Image.open(filename))
+#             ben_strings = ben.splitlines()
+#             return_obj['eng'] = [x.strip() for x in ben_strings if x.strip()]
 
-        return_obj = {}
-        if "ben" in ocr_model:
-            ben = pytesseract.image_to_string(Image.open(filename), lang='ben')
-            ben_strings = ben.splitlines()
-            return_obj['ben'] = [x.strip() for x in ben_strings if x.strip()]
-
-        if "bengali" in ocr_model:  
-            bengali = pytesseract.image_to_string(Image.open(filename), lang='Bengali')
-            bengali_strings = bengali.splitlines()
-            return_obj['bengali'] = [x.strip() for x in bengali_strings if x.strip()]
-
-        return return_obj
-    except Exception as error:
-        print('error', error)
-
-
-
-def onlyeng(filename, threshold, medianBlur, blurRatio, ocr_model):
-    try:
-        image = cv2.imread(filename)
-
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # if medianBlur == 1:
-        #     # print(blurRatio%2)
-        #     if blurRatio > 0 and blurRatio%2 != 0:
-        #         gray = cv2.medianBlur(gray, blurRatio)
-        # if threshold == 1:
-        #     gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-        gray = cv2.medianBlur(gray, blurRatio)
-        gray = cv2.GaussianBlur(gray, (1, 1), 0)
-        # gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 139, 25.0)
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
-        gray = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-
-        # gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 139, 25.0)
-        
-
-
-        filename = 'process_image/image.jpg'
-        cv2.imwrite(filename, gray)
-
-        return_obj = {}
-        if "ben" in ocr_model:
-            ben = pytesseract.image_to_string(Image.open(filename))
-            ben_strings = ben.splitlines()
-            return_obj['eng'] = [x.strip() for x in ben_strings if x.strip()]
-
-        # if "bengali" in ocr_model:  
-        #     bengali = pytesseract.image_to_string(Image.open(filename), lang='Bengali')
-        #     bengali_strings = bengali.splitlines()
-        #     return_obj['bengali'] = [x.strip() for x in bengali_strings if x.strip()]
-
-        return return_obj
-    except Exception as error:
-        print('error', error)
+#         return return_obj
+#     except Exception as error:
+#         print('error', error)
 
 def test_all_file():
     path = '/home/nilim/Downloads/NID_Front_Image'
@@ -138,19 +191,37 @@ def test_all_file():
 
     for filename in files:
         # result = preImgToOcr(filename, 1, 1, 1, ['ben', 'bengali'])
-        result = imageToOcr(filename, 1, 1, 1, ['ben', 'bengali'])
+        result = processImgToOcr(filename, 0, 1, 1, 1, ['ben', 'bengali'])
         result['name'] = filename
 
         print('result', result)
-        f = open("eng.txt", "a")
+        f = open("process-02.txt", "a")
         f.write(str(result) + '\n')
         f.close()
 
 
-test_all_file()
+if __name__ == "__main__":
+    test_all_file()
 
-# 01847418952
+# def backup():
+#     img = cv2.imread(filename)
+#     img = cv2.bilateralFilter(img, 9, 31, 31)
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+#     gray = cv2.medianBlur(gray, 1)
+#     gray = cv2.GaussianBlur(gray, (1, 1), 0)
+#     thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 139, 25.0)
+#     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+#     gray = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
 
-# 01911780554_4.png
+# gray = cv2.resize(gray, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+# gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+# gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+
+
+# ocr_01.jpg
+# 01921011814.png
 # 01911780554_3.jpg
+# 01921011823.png
+# 01848622592.png
